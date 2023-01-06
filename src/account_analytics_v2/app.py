fb234e192cce28200808a55f379415e9cbeffcf4
tndrcloud <alexbardysov96@gmail.com>
2023-01-06 14:39:24 +0300

    update app.py

--- src/account_analytics_v2/app.py
+++ src/account_analytics_v2/app.py
@@ -4,18 +4,20 @@ from .api import router
 
 tags_metadata = [
     {
-        'name': 'Authorization and authentication',
+        'name': 'auth',
+        'description': 'Authorization and authentication'
     },
     {
-        'name': 'Operations',
+        'name': 'operations',
+        'description': 'Operations'
     },
 ]
 
 
 app = FastAPI(
-    title='Анализатор ЛК ВАТС',
-    description='REST API сервис для проверки корректности настроек и поиска ошибок в логах вызовов ЛК ВАТС',
-    version='v2.0',
+    title='Personal Account Analytics',
+    description='Cервис для проверки корректности настроек и поиска ошибок в логах вызовов ЛК ВАТС',
+    version='v2',
     openapi_tags=tags_metadata
 )
 app.include_router(router)
